12c15ea8cd3931bdb0f909d48d896a50e3ca1842
muhnashir <muhammadnashir9@gmail.com>
2022-07-21 14:46:26 +0700

    [Transaction] get transaksi by id campaign

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,39 @@
 package transaction
+
+import (
+	"bwastartup/campaign"
+	"errors"
+)
+
+
+
+type service struct{
+	repository Repository
+	campaignRepository campaign.Repository
+}
+
+
+func NewService(repository Repository, campaignRepository campaign.Repository)*service{
+	return &service{repository, campaignRepository}
+}
+
+
+type Service interface{
+	GetTransactionsByCampaignID(input GetCampaignTransactionInput)([]Transaction, error)
+}
+
+func(s *service) GetTransactionsByCampaignID(input GetCampaignTransactionInput)([]Transaction, error){
+	campaign, err:= s.campaignRepository.FindByID(input.ID)
+	if err != nil {
+		return []Transaction{}, err
+	}
+	if campaign.UserID != input.User.ID {
+		return []Transaction{}, errors.New("Not an owner of the campaign")
+	}
+	
+	transaction, err:= s.repository.GetByCampaignByID(input.ID)
+	if err != nil {
+		return transaction, err
+	}
+	return transaction, nil
+}
